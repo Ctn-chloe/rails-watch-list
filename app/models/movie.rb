cb31bf56fbdb4bd1835ae141f00c20a4f2fc729e
Chloe Cartron <chloe.cartron@gmail.com>
2022-05-19 13:02:36 +0200

    add validation and associations to models

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,2 +1,14 @@
 class Movie < ApplicationRecord
+  has_many :bookmarks
+  before_destroy :check_for_bookmarks
+
+  validates :title, uniqueness: true
+  validates :overview, presence: true
+  validates :title, length: { minimum: 1 }
+
+  private
+
+  def check_for_bookmarks
+    bookmarks.count.positive? ? false : true
+  end
 end
